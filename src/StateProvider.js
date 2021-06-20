6a4942a4f6607ee58e90e99f9608235d9e770df1
bhargab236 <68290265+bhargab236@users.noreply.github.com>
2021-06-20 16:11:11 +0530

    made 3 changes

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,1 +1,16 @@
-//bhargab
+//setup data layer
+//we need this to track the basket
+
+import React, { createContext, useContext, useReducer } from "react";
+
+//this is the data layer
+export const StateContext = createContext();
+
+//BUILD A PROVIDER
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
